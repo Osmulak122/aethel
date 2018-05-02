9d9c73cf711e1a517a130f85f3e013c91405e251
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2018-05-02 14:20:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 var bot = new Discord.Client();
-const Client = require('fortnite');
-const fortnite = new Client('3042528c-188c-41f8-9e75-51b3e7288cd4');
 const prefix = "!";
+var request = require('request');
+
 
 bot.on("guildMemberAdd", function(member) {
     var role_rules = member.guild.roles.find('name', 'Unaccepted Rules');
@@ -39,18 +39,19 @@ bot.on("message", function(message) {
     
     if (message.channel == channel1) {
         message.delete();
-    }  
+    }
+   
     if (msg == prefix + "help") {
         message.channel.send("__**Commands :**__\n\n**!youtube**\n**!leaders**\n**!botcode**\n\n__**Music:**__\n**!musicbot** *Some music commands*");
     }
     if (msg == prefix + "leaders") {
-        message.channel.send("**Leaders:**\n*MR3K*\n*Minty*");
+        message.channel.send("**Leaders:**\n*MR3K*\n*Minty*\n*JdL*");
     }                    
     if(msg == prefix + "botcode") {
         message.channel.send("**Actual bot code! -** https://github.com/Osmulak122/aethel/blob/master/index.js");
     }
      if(msg == prefix + "youtube") {
-        message.channel.send("**Our Youtube! -** https://www.youtube.com/c/AethelFortnite");
+        message.channel.send("**Our Youtube! -** https://www.youtube.com/AethelFortnite");
     }
 
     if(msg.startsWith(prefix + "clear")) {
@@ -75,7 +76,7 @@ bot.on("message", function(message) {
             message.channel.bulkDelete(fetched)
                 .catch(error => message.channel.send("Can't clear the chat!"));
 
-                bot.channels.find("name", "bot-logs").send(msgauthor.username + "** cleared : **" + (fetched.size) + "** messages in channel :** " + (channelID));
+                bot.channels.find("name", "bot-logs").send(`**${msgauthor.username}**` + " cleared : " + `**${fetched.size}**` + "** messages in channel :** " + (channelID));
 
  }
         clear();
@@ -93,29 +94,80 @@ bot.on("message", function(message) {
                     .addField("?shuffle", "Shuffles Queue",true)
                     .addField("?queue", "Shows current queue", true)
                     .setFooter('Remember that music commands are executed with "?" prefix.')
-                    .setColor(0xE6A12D)
+                    .setColor(0xFF2017)
                     
                 message.channel.send(musichelp);
         }
             if(msg == prefix + "roster") {
                 var roster = new Discord.RichEmbed()
-                    .setDescription("**Aethel Roster:**")
+                    .setDescription("Aethel Roster")
                     .setThumbnail(bot.guilds.find("id", "440424875346231296").iconURL)
-                    .addField("Leaders", "*MR3K\nMinty\nJdL*")
-                    .addField("Players", "*Player\nPlayer\nPlayer\nPlayer*")
-                    .addField("Designers","*Genizz\nDesigner\nDesigner\nDesigner*")
-                    .addField("Editors", "*Editor\nEditor\nEditor*")
+                    .addField("Leaders", "**MR3K\nMinty\nJdL**")
+                    .addField("Players","KrKs")
+                    .addField("Designers","Genizz")
+                    .addField("Editors")
                     .setFooter('Roster might not be up to date')
                     .setColor(0xFF2017)
                     message.channel.send(roster);
             }
-             if(msg.startsWith("!stats")) {
-                let username = args[0];
-                let platform = "pc";
-                fortnite.user(username, platform).then(data => console.log(data));                  
-            }                    
-});   
-    bot.login(process.env.BOT_TOKEN);
+            if(msg.startsWith("!stats")) {
+                let platform = args[1] || "pc";
+                var playerName = args[0];
+
+                var options = {
+                method: "GET",
+                url: `https://fortnite.y3n.co/v2/player/${playerName}`,
+                headers: {
+                    'User-Agent': 'nodejs request',
+                    'X-Key': "c77fTff744ovhhm4b34l"
+                }
+            }  
+            request(options, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    var stats = JSON.parse(body);
+
+                    //statsembed
+
+                    var statsembed = new Discord.RichEmbed()
+                        .setTitle("Fortnite Stats")
+                        .setAuthor(playerName)
+                        .addField(`Wins`, `${stats.br.stats.pc.all.wins}`, true)
+                        .addField(`Win Rate`, `${stats.br.stats.pc.all.winRate}`, true)
+                        .addField(`Total Score`, `${stats.br.stats.pc.all.score}`, true)
+                        .addField(`Kills`, `${stats.br.stats.pc.all.kills}`, true)
+                        .addField(`KD`, `${stats.br.stats.pc.all.kpd}`, true)
+                        .addField(`Matches Played`, `${stats.br.stats.pc.all.matchesPlayed}`, true)
+                        .setColor(0xFF2017)
+                        .setThumbnail(message.author.avatarURL)
+                        .setFooter(`Last stats update :  ${stats.lastUpdate}`)
+
+                        message.channel.send(statsembed);
+
+                } else {
+                    console.log(error);
+                }
+            })    
+            
+        }
+        if(msg == prefix + "servers") {
+            var options2 = {
+                method: "GET",
+                url: `https://fortnite.y3n.co/v2/gamestatus`,
+                headers: {
+                    'User-Agent': 'nodejs request',
+                    'X-Key': "c77fTff744ovhhm4b34l"
+                }
+            }
+            request(options2, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    var stats = JSON.parse(body);
+                    message.channel.send(`Fortnite servers are **${stats.status}**`)    
+            }  else {
+                console.log(error);
+            }
+        })
+    }
 
 
-
+});      
+bot.login(process.env.BOT_TOKEN);
